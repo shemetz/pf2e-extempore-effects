e23c7887a0c3adb1ce58dec986e6124fd9378281
itamar <itamarcu@gmail.com>
2024-06-15 23:07:25 +0300

    Revert "Removed "only_unidentified" option from effect expiration reminders"
    This reverts commit 3399adfd0d1742c4fbabe1306981ef41a92aba85.

--- scripts/pf2e-extempore-effects.js
+++ scripts/pf2e-extempore-effects.js
@@ -59,9 +59,10 @@ Hooks.on('init', () => {
     scope: 'world',
     config: true,
     type: String,
-    default: 'all_effects',
+    default: 'only_unidentified',
     choices: {
       'all_effects': localize('.settings.notifications-for-expired-effects.choice_all_effects'),
+      'only_unidentified': localize('.settings.notifications-for-expired-effects.choice_only_unidentified'),
       'disabled': localize('.settings.notifications-for-expired-effects.choice_disabled'),
     },
   })
@@ -196,6 +197,7 @@ const onUpdateWorldTime_Wrapper = (wrapped, ...args) => {
     if (effectNotificationSetting === 'disabled') break
     if (effect.isExpired) continue
     const isSecretEffect = effect.system.unidentified
+    if (!isSecretEffect && effectNotificationSetting === 'only_unidentified') continue
     const effectExpiryTimeLux = oldWorldTimeLux.plus({ seconds: effect.remainingDuration.remaining })
     const isEffectGonnaExpireNow = effectExpiryTimeLux.startOf('second') <= newWorldTimeLux.startOf('second')
     if (isEffectGonnaExpireNow) {
